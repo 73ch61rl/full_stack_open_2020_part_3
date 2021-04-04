27edad41757cb338f97a699772b90f185c7e8513
Irina Koski <irina.smirnova86@gmail.com>
2021-04-04 14:16:25 +0300

    3.6: Phonebook backend step6

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 let persons = [
-        {
-          "id": 1,
-          "name": "Arto Hellas",
-          "number": "040-123456"
-        },
-        {
-          "id": 2,
-          "name": "Ada Lovelace",
-          "number": "39-44-5323523"
-        },
-        {
-          "id": 3,
-          "name": "Dan Abramov",
-          "number": "12-43-234345"
-        },
-        {
-          "id": 4,
-          "name": "Mary Poppendieck",
-          "number": "39-23-6423122"
-        }
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
 ]
 
 app.get('/api/persons', (request, response) => {
@@ -30,7 +30,7 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/info', (request, response) => {
     response.send(
-    `<p>Phonebook has info for ${persons.length} people.</p>
+        `<p>Phonebook has info for ${persons.length} people.</p>
      <p>${new Date()}.</p>`)
 })
 
@@ -57,18 +57,26 @@ const generatePersonId = () => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    if (!body.name) {
+    const duplicatedName = persons.find(person => person.name === body.name)
+    if (!body.name || !body.number) {
         return response.status(400).json({
-            error: 'The request cannot be fulfilled due to bad syntax.'
+            error: "Person's name and number are required!"
         })
     }
-    const person = {
-        id: generatePersonId(),
-        name: body.name,
-        number: body.number,
+    else if (duplicatedName) {
+        return response.status(400).json({
+            error: "A person with this name already exists! Choose a different name!"
+        });
     }
-    persons = persons.concat(person)
-    response.json(person)
+    else {
+        const person = {
+            id: generatePersonId(),
+            name: body.name,
+            number: body.number,
+        }
+        persons = persons.concat(person)
+        response.json(person)
+    }
 })
 
 const PORT = 3001
